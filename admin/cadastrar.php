cd3ee76e396dcd7c8e261fd8a35e1419867a35a5
Raiaane <43380217+Raiaane@users.noreply.github.com>
2018-10-01 14:20:04 -0300

    Add files via upload

--- admin/cadastrar.php
+++ admin/cadastrar.php
@@ -4,56 +4,75 @@
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css">
 </head>
-
 <?php
 error_reporting(0);
-
-
 $nomep = $_POST['nomep'];
 $quant = $_POST['quant'];
 $valoru = $_POST['valoru'];
 $descri = $_POST['descri'];
+$categ = $_POST['cate'];
 
+$dir = "img/";
 
-$sql = "INSERT INTO produtos (produto , quantidade, preco , descricao) VALUES ('$nomep' , '$quant' , '$valoru' , '$descri')";
-
+$sql = "INSERT INTO produtos VALUES (default,'$nomep' , '$quant' , '$valoru' , '$descri' , '$categ', '0')";
 $res = mysqli_query($conexao, $sql);
+
+$sqlPg = "SELECT LAST_INSERT_ID()";
+$queryPg = mysqli_query($conexao, $sqlPg);
+
+	if ($queryPg) {
+	$codPgIndice = mysqli_fetch_row($queryPg);
+	$salva = $codPgIndice[0];	
+
 	if ($res) {
 
 		$imagem = $_POST['img'];
-		$id_p = mysqli_fetch_row("SELECT LAST_INSERT_ID()");
+	
+		if (isset($_FILES['img']) && isset($_FILES['img2']) && isset($_FILES['img3'])) {
 
-		echo "<script> alert('".$id_p."') </script>";
+			$nome1 = microtime();
+			$nome2 = md5(microtime());
+			$nome3 = base64_encode(microtime());
 
-		$msg = false;
+			$ext = strtolower(substr($_FILES['img']['name'],-4));
+			$novo_nome = $nome1.$ext;
 
-		if (isset($_FILES['img'])) {
-			$extensao = strtolower(substr($_FILES['img']['name'] , -4));
-			$novo_nome = md5(microtime()).$extensao;
-			$diretorio = "upload/";
+			$ext2 = strtolower(substr($_FILES['img2']['name'],-4));
+			$novo_nome2 = $nome2.$ext2;
 
-			move_uploaded_file($_FILES, ['arquivo']['tpm_name'] .$diretorio . $novo_nome);
+			$ext3 = strtolower(substr($_FILES['img3']['name'],-4));
+			$novo_nome3 = $nome3.$ext3;
+ 
+			$sal = move_uploaded_file($_FILES['img']['tmp_name'], $dir.$novo_nome);
+			$sal2 = move_uploaded_file($_FILES['img2']['tmp_name'], $dir.$novo_nome2);
+			$sal3 = move_uploaded_file($_FILES['img3']['tmp_name'], $dir.$novo_nome3);
+			
+			if ($sal & $sal2 & $sal3) {
+				echo "<script type='text/javascript'> alert('imagem') </script>";
+			}
 
-			$sql_img = "INSERT INTO imagem (id_produto , imagem) VALUES ('$id_p', '$novo_nome')";
+			$sql_img = "INSERT INTO imagem (id, id_produto, imagem) VALUES (default, '".
+              $salva."', '$novo_nome')";
 			$queryImg = mysqli_query($conexao, $sql_img);
 
-			$cat = $_POST['cate'];
-			$sql_cat =  "INSERT INTO categoria (categoria , id_produto) VALUES ('$cat' , $id_p);";
-			$queryCat = mysqli_query($conexao, $sql_cat);
-$cores = $_POST['cor'];
+			$sql_img2 = "INSERT INTO imagem (id, id_produto, imagem) VALUES (default, '".
+              $salva."', '$novo_nome2')";
+			$queryImg2 = mysqli_query($conexao, $sql_img2);
 
-$sql_cor =  "INSERT INTO cor (cor , id_produto) VALUES ('$cores' , $id_p);";
-			$querycor = mysqli_query($conexao, $sql_cor);
+			$sql_img3 = "INSERT INTO imagem (id, id_produto, imagem) VALUES (default, '".
+              $salva."', '$novo_nome3')";
+			$queryImg3 = mysqli_query($conexao, $sql_img3);
 
-	if($queryImg){
-			echo "Foi"; 
-		}
+			if($queryImg){
+				echo "Foi"; 
+			}
 
-		echo "<script>alert('Cadastrado!!')</script>";
-	} else {
-		echo "<script>alert('Erro!!')</script>";		
-	}
+				echo "<script>alert('Cadastrado!!')</script>";
+			} else {
+				echo "<script>alert('Erro!')</script>";		
+			}
 
+}
 }
 
  ?>
@@ -69,49 +88,50 @@ $sql_cor =  "INSERT INTO cor (cor , id_produto) VALUES ('$cores' , $id_p);";
 				<form method="post" enctype="multipart/form-data">
 					<div class="form-group">
 						<label>Nome do produto</label>
-						<input placeholder="Nome do produto" class="form-control" type="text" name="nomep">
+						<input placeholder="Nome do produto" class="form-control" type="text" name="nomep" style="width: 2000px;">
 					</div>
 					<div class="form-group">
 						<label>Valor por unidade</label>
 						<div class="input-group col-md-4">
-					<input placeholder="Valor unitário" class="form-control" type="number" name="valoru">
+					<input placeholder="Valor unitário" class="form-control" type="number" name="valoru" style="width: 2000px;">
 						</div>
 					</div>
 					<div class="form-group">
 						<label>Quantidade</label>
 						<div class="input-group col-md-4">
-							<input placeholder="Quantidade" class="form-control" type="text" name="quant">
+							<input placeholder="Quantidade" class="form-control" type="text" name="quant" style="width: 2000px;">
 						</div>
 					</div>
 						<div class="form-group">
 						<label>Descrição</label>
-						<textarea class="form-control" placeholder="Descrição" style="max-width: 560px; min-width: 560px; max-height: 150px; min-height: 100px;" name="descri"></textarea>
+						<textarea class="form-control" placeholder="Descrição" style="max-width: 2000px; min-width: 560px; max-height: 150px; min-height: 100px; width: 2000px;" name="descri" ></textarea>
 						</div>
 					 <div class="form-group">
 						<label>Foto</label>
 						<input type="file" name="img">
+						<label>Foto - 2</label>
+						<input type="file" name="img2">
+						<label>Foto - 3</label>
+						<input type="file" name="img3">
 						</div>
 					<div class="form-group">
-						<label>Categoria: </label>
-						<select name="cate">
-							<option>infantil</option>
-							<option>PP</option>
-							<option>P</option>
-							<option>M</option>
-							<option>G</option>
-							<option>GG</option> 
-						    </select></div> </br>
-						    <div class="form-group">
-							<label>Cor: </label>
-							<select name="cor">
-							<option>Branco</option>
-							<option>Preto</option>
-							<option>Azul</option>
-							<option>Cinza</option>
-							<option>Laranja</option>
-							<option>Vermelho</option> 
-						    </select></div> </br>		
-					<input class="btn btn-success" value="Cadastrar" type="submit" name="submit">
+						<label>Categoria: </label></br>
+						<select name="cate" style="width: 2000px; height: 30px;">
+							<?php
+
+								$sqlCatMostra = "SELECT * FROM categoria;";
+								$queryCatMostra = mysqli_query($conexao, $sqlCatMostra);
+
+								while ($cat = mysqli_fetch_assoc($queryCatMostra)) {
+									
+									echo "<option value='".$cat['categoria']."'>".$cat['categoria']."</option>";
+
+								}
+
+							?> 
+						</select><br>
+
+					<input class="btn btn-success" value="Cadastrar" type="submit" name="submit" style="width: 500px; margin-left: 150px;">
 					<br><br>
 				</form>
 			</div>
